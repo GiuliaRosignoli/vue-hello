082c3e1a44d0c7df2095629a798b4700ee6ce2d8
GiuliaRosignoli <giuliarosignoli@gmail.com>
2021-04-20 16:54:48 +0200

    .

---
+++ script/main.js
@@ -0,0 +1,11 @@
+// console.log("Vue: " , Vue);
+
+
+const app = new Vue ({
+    el: "#app",
+    data: {
+        myClass: "myClass",
+        title: "Hi, there! Welcome to Vue.js App.",
+        image: "./img/shirleyslade.jpg",
+    }
+});
